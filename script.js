49b2257cf959b7a8bafc089d8c86e13f9f9164e4
Dikshyanta Aryal <adikshyanta456@gmail.com>
2022-05-30 09:32:44 +0545

    navbar

--- script.js
+++
@@ -1,9 +0,0 @@
-function parallax(e) {
-  this.querySelectorAll('.elements').forEach(layer => {
-    const speed = layer.getAttribute('data-speed');
-    const x = (window.innerWidth - e.pageX*speed)/100;
-    layer.style.transform = `translateX(${x}px)`;
-  });
-}
-
-document.addEventListener("mousemove",parallax);
